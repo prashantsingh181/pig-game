14cf1e77b1d1e00f58dad655dd362d32835a813b
prashantsingh181 <prashant.singh181@gmail.com>
2024-10-05 23:49:43 +0530

    added moda

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const WINNING_SCORE = 20;
 const RESET_SCORE = [1, 6];
 
+let playerNames = ['Player 1', 'Player 2'];
 let currentScore;
 let score = [0, 0];
 let isFirstPlayerPlaying = true;
@@ -13,9 +14,52 @@ const player1Section = document.querySelector('.player--1');
 const diceRollButton = document.querySelector('.btn--roll');
 const holdButton = document.querySelector('.btn--hold');
 
+const formData = `
+      <form class="register-form">
+        <div class="description">
+          <h2 class="modal-title">Welcome to Pig Dice Game!</h2>
+          <h3>Rules</h3>
+          <ul>
+            <li>Each player takes turns rolling a die.</li>
+            <li>Players can accumulate their scores.</li>
+            <li>Cursed Numbers: ${RESET_SCORE.join(
+              ', '
+            )} - If a player rolls a cursed number, they lose their current score, and the next player takes their turn.</li>
+            <li>Players can choose to hold their score, securing it but giving the next player a chance to roll.</li>
+            <li>The first player to reach ${WINNING_SCORE} wins.</li>
+          </ul>
+        </div>
+        
+        <h4>Please enter your names for each player.</h4>
+
+        <div class="modal-inputs">
+          <input
+            type="text"
+            name="player-name--0"
+            placeholder="Enter Player 1 name"
+            required
+          />
+          <input
+            type="text"
+            name="player-name--1"
+            placeholder="Enter Player 2 name"
+            required
+          />
+        </div>
+
+        <div class="modal-buttons">
+          <button class="modal-btn btn--skip" type="button">Skip</button>
+          <button class="modal-btn btn--save" type="submit">Save</button>
+        </div>
+      </form>`;
+
 function init() {
   score = [0, 0];
+  playerNames = ['Player 1', 'Player 2'];
   currentScore = 0;
+  document.querySelectorAll('.name').forEach((nameEl, index) => {
+    nameEl.textContent = playerNames[index];
+  });
   document
     .querySelectorAll('.score')
     .forEach((scoreEl, index) => (scoreEl.textContent = score[index]));
@@ -30,6 +74,39 @@ function init() {
   dice.classList.add('hidden');
   diceRollButton.classList.remove('hidden');
   holdButton.classList.remove('hidden');
+  showModal(formData);
+  document
+    .querySelector('.register-form')
+    .addEventListener('submit', handleSubmit);
+  document.querySelector('.btn--skip').addEventListener('click', handleSkip);
+}
+
+function hideModal() {
+  document.querySelector('.modal').style.display = 'none';
+  document.querySelector('.overlay').style.display = 'none';
+  document.body.style.overflow = 'auto';
+}
+
+function showModal(content) {
+  document.querySelector('.modal').style.display = 'block';
+  document.querySelector('.overlay').style.display = 'block';
+  document.querySelector('.modal').innerHTML = content;
+
+  document.body.style.overflow = 'hidden';
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const inputs = e.target.querySelectorAll('input');
+  inputs.forEach((input, index) => {
+    playerNames[index] = input.value;
+    document.getElementById(`name--${index}`).textContent = playerNames[index];
+  });
+  hideModal();
+}
+
+function handleSkip() {
+  hideModal();
 }
 
 function changeTurn() {
